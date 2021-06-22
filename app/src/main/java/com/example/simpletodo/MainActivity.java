de19672d5ed602fb881091d13c1fa121f385e227
Ricardo Glez Leal <53849239+RicardoGLeal@users.noreply.github.com>
2021-06-22 17:10:43 -0500

    implemented persistence
    -Now you can save and load the toDo List thanks to the implementation of persistence

--- app/src/main/java/com/example/simpletodo/MainActivity.java
+++ app/src/main/java/com/example/simpletodo/MainActivity.java
@@ -5,11 +5,17 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -29,11 +35,7 @@ public class MainActivity extends AppCompatActivity {
         item_et = findViewById(R.id.etItem);
         items_rv = findViewById(R.id.items_rv);
 
-        item_et.setText("Hello World!");
-
-        items = new ArrayList<>();
-        items.add("Play with my friends");
-        items.add("Take the Android Course");
+        loadItems();
 
         ItemsAdapter.OnLongClickListener onLongClickListener = new ItemsAdapter.OnLongClickListener() {
 
@@ -44,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
                 //Notify the adapter
                 itemsAdapter.notifyItemRemoved(position);
                 Toast.makeText(getApplicationContext(), "Item was removed", Toast.LENGTH_SHORT).show();
+                saveItems();
             }
         };
         itemsAdapter = new ItemsAdapter(items, onLongClickListener);
@@ -60,7 +63,30 @@ public class MainActivity extends AppCompatActivity {
                 itemsAdapter.notifyItemInserted(items.size()-1);
                 item_et.setText("");
                 Toast.makeText(getApplicationContext(), "Item was added", Toast.LENGTH_SHORT).show();
+                saveItems();
             }
         });
+    }
+
+    private File getDataFile() {
+        return new File(getFilesDir(),"data.txt");
+    }
+
+    //This function will load items by reading every line of the data file
+    private void loadItems() {
+        try {
+            items = new ArrayList<>(FileUtils.readLines(getDataFile(),Charset.defaultCharset()));
+        } catch (IOException e) {
+            Log.e("MainActivity", "Error Reading Items", e);
+            items = new ArrayList<>();
+        }
+    }
+    //This function saves items by writing them into the data file
+    public void saveItems() {
+        try {
+            FileUtils.writeLines(getDataFile(),items);
+        } catch (IOException e) {
+            Log.e("MainActivity", "Error writing Items", e);
+        }
     }
 }
